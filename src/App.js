0753b01d23b93363e274ac815964ff5c17860ac9
Sandeep Ganguly <sandeep@example.com>
2021-04-08 20:01:05 +0530

    footer setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {
   BrowserRouter as Router
 } from 'react-router-dom'
+
 import NavbarContainer from './components/Layout/Navbar/NavbarContainer'
+import Footer from './components/Layout/Footer'
 
 export default function App() {
   return (
     <Router>
       <NavbarContainer />
+      <Footer />
     </Router>
   )
 }
---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="container-fluid my-5 py-5 bg-dark">
+      <div className="container text-center text-white">
+        <img src="assets/img/slack_logo.png" className="rounded-circle me-2" width="40" height="40" />
+        Hack Slack App @ 2021
+      </div>
+    </div>
+  )
+}
+
+export default Footer
