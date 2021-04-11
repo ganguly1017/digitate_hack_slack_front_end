b552815edfa2297f9e01c59ba8ad8d3861d6dc25
Sandeep Ganguly <sandeep@example.com>
2021-04-11 19:54:05 +0530

    update

--- src/components/Layout/TeamList/TeamCard.js
+++ src/components/Layout/TeamList/TeamCard.js
@@ -9,7 +9,6 @@ function TeamCard(props) {
   } = props
   return (
     <div className="card shadow animate__animated animate__rubberBand p-3 mb-5 bg-body rounded" >
-
       <div className="card-body">
         <div className="row">
           <div className="col-md-4">
